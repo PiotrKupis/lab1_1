d9cab4377390ef0c8c5dbb5f61419a5dddbe8ece
Piotr.Kupis <222488@edu.p.pldz.pl>
2021-03-08 18:53:20 +0100

    Replaced product's fields in OffertItem with Product object

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -1,6 +1,7 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Money {
     private BigDecimal amount;
@@ -9,6 +10,20 @@ public class Money {
     public Money(BigDecimal amount, String currency) {
         this.amount = amount;
         this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount.equals(money.amount) &&
+                currency.equals(money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
     }
 
     public BigDecimal getAmount() {
